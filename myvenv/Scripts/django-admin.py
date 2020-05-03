76688e0864acbce96eb655ade02529929fca3f60
ajayjaswani <ajayjaswani96@gmail.com>
2020-05-03 20:18:42 +0530

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ajay jaswani\documents\python-projects\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
